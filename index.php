68bf525cd64f877f25e678045eb08cf31a261e00
Biplop Maharjan <maharjankija@gmail.com>
2026-02-02 10:06:16 -0700

    just started

---
+++ index.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+ <html>
+    <head>
+        <title>Kija Growth</title>
+        
+    </head>
+    <body>
+    
+        <?php
+            include 'home/home.php';
+            include 'data/db.php';
+            include 'css/bootstrap_css.php';
+            include 'java/bootstrap_js.php';
+        ?>
+    </body>
+</html>
